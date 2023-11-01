f37e7f75897cd86be22e9c2d0faa5348903b4183
ANDREA C <andrea_coronado20212@elpoli.edu.co>
2023-11-01 09:17:30 -0500

    feat(fontend):
    se crea pantalla recuprar.php, para recuperar la contrseña

---
+++ recuperar.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recuperar Contraseña</title>
+</head>
+<body>
+
+<div class="container">
+    <h2>Recuperar Contraseña</h2>
+    <form action="procesar_recuperacion.php" method="POST">
+        <label for="email">Ingresar Email:</label>
+        <input type="email" name="email" id="email" required>
+        <button type="submit">Enviar</button>
+    </form>
+</div>
+
+</body>
+</html>
+ 
