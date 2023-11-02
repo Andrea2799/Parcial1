ac580a6df34de91d771006154207fd35b1ac7c55
MiguelOrtiz <Miguel_Ortiz82192@elpoli.edu.co>
2023-11-02 13:51:30 -0500

    HU4 CREAR PERFIL
    feat(frontend):
    
    se agregan algunas cosas, falta lo ultimo configuraciones

--- conexion.php
+++ conexion.php
@@ -4,18 +4,17 @@ $username = "root";
 $password = "";
 $dbname = "usuarios";
 
-// Crear la conexión
 $conexion = new mysqli($servername, $username, $password, $database);
 
-// Verificar la conexión
+
 if ($conexion->connect_error) {
     die("Conexión fallida: " . $conexion->connect_error);
 }
 
-// Seleccionar base de datos
+
 $conexion->select_db($database);
 
-// Las siguientes variables representan el nombre de la tabla y los nombres de las columnas
+
 $tablaUsuarios = "usuarios";
 $columnaNuevaPassword = "new_password";
 $columnaConfirmarPassword = "confirm_password";
@@ -24,7 +23,7 @@ $columnaEmail = "email";
 $columnaPassword = "password";
 $columnaUser = "user";
 
-// Puedes realizar consultas a la tabla de usuarios con estas nuevas columnas
+
 
 ?>
 
