66cd4c8e0a04121c09d821028392a6ecd9b216e3
andrea <andrea_coronado20212@elpoli.edu.co>
2023-12-02 13:03:34 -0500

    build (backend):
    se edita el login para poder identificar los reoles de adminitsrador y cliente , sie es administrador dirige a una pagina especifica

--- php/login_user.php
+++ php/login_user.php
@@ -1,5 +1,5 @@
 <?php
-include 'modconn.php'; // Asegúrate de que esta inclusión apunte al archivo correcto de conexión a la base de datos
+include 'modconn.php';
 
 $email = $_POST['email'];
 $pass = $_POST['pass'];
@@ -9,10 +9,26 @@ if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
 
-$validar_login = mysqli_query($conn, "SELECT * FROM usuarios WHERE email = '$email' AND pass = '$pass'");
+$validar_login = mysqli_query($conn, "SELECT id_cargo FROM usuarios WHERE email = '$email' AND pass = '$pass'");
 
 if(mysqli_num_rows($validar_login) > 0){
-    header("location: ../indexmod.html");
+    $fila = mysqli_fetch_assoc($validar_login);
+    $id_cargo = $fila['id_cargo'];
+
+    // Redirección basada en el valor de id_cargo
+    if ($id_cargo == 2) {
+        header("Location: ../indexmod.html");
+    } elseif ($id_cargo == 1) {
+        header("Location: ../indexmod_admin.html");
+    } else {
+        echo '
+            <script>
+                alert("Invalid id_cargo");
+                window.location = "../loginmod.html";
+            </script>
+        ';
+        exit();
+    }
 } else {
     echo '
         <script>
